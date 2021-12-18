4523ca426ac3310a9b844e3e7b4aec891f777b32
benjaminhonorio <benjaminhonorio.12@gmail.com>
2021-12-18 11:00:29 -0500

    Add some styles to ProDuct Detail

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,21 +1,38 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { Rating } from 'react-simple-star-rating';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTag } from '@fortawesome/free-solid-svg-icons';
 
 export default function ProductDetail({ products }) {
   let params = useParams();
   const product = products.find((p) => p.id === Number(params.productId));
   console.log('renders from product detail');
+
   if (product) {
     return (
       <>
-        <h1>{product ? product.title : null}</h1>
-        <p>
-          {product.rating.rate} from {product.rating.count} reviews
-        </p>
-        <p>{product.category}</p>
-        {/* <img src={product.image} /> */}
-        <p>Price: $ {product.price}</p>
-        <h2>Description</h2>
-        <p>{product.description}</p>
+        <div className="product__detail">
+          <h2>{product ? product.title : null}</h2>
+
+          <div>
+            <Rating
+              size={`1.25rem`}
+              initialValue={Math.round(product.rating.rate)}
+              readonly={true}
+            />
+            {product.rating.count} reviews
+          </div>
+          <img src={product.image} />
+
+          <h2>Price: $ {product.price}</h2>
+
+          <h3>Description</h3>
+          <p>{product.description}</p>
+          <div>
+            <FontAwesomeIcon icon={faTag} /> {product.category}
+          </div>
+        </div>
       </>
     );
   } else {
